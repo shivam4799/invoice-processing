caf6166e206903776bf436a446b8dfe1e27f0c72
shivam patel <shivam@glib.ai>
2020-08-14 13:23:29 +0530

    push to git

--- invoice/forms.py
+++ invoice/forms.py
@@ -15,18 +15,18 @@ class InvoicesForm(forms.ModelForm):
         exclude = ('created_by',)
 
         widgets = {
-            'invoice_no': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'required': True,'id':'invoice_no_input',},),
-            'invoice_date': forms.DateInput(attrs={'class': 'form-control form-control-sm', 'required': True,'id':'invoice_date_input'}),
-            'gstin': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'required': True,'id':'gstin_input'}),
-            'vendor_name': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'required': True,'id':'vendor_name_input'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control form-control-sm', 'required': True,'id':'email_input'}),
-            'phone_number': forms.NumberInput(attrs={'class': 'form-control form-control-sm', 'required': True,'id':'phone_number_input'}),
-            'imei': forms.NumberInput(attrs={'class': 'form-control form-control-sm', 'required': True,'id':'imei_input'}),
-            'vendor_address': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'required': True,'id':'vendor_address_input'}),
-            'total_taxable_amount': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'required': True, 'id': 'vendor_address_input'}),
-            'sgst': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'required': True, 'id': 'vendor_address_input'}),
-            'igst': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'required': True, 'id': 'vendor_address_input'}),
-            'cgst': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'required': True, 'id': 'vendor_address_input'}),
-            'total_amount': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'required': True, 'id': 'vendor_address_input'}),
+            'invoice_no': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'id':'invoice_no_input',},),
+            'invoice_date': forms.DateInput(attrs={'class': 'form-control form-control-sm', 'id':'invoice_date_input'}),
+            'gstin': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'id':'gstin_input'}),
+            'vendor_name': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'id':'vendor_name_input'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control form-control-sm', 'id':'email_input'}),
+            'phone_number': forms.NumberInput(attrs={'class': 'form-control form-control-sm', 'id':'phone_number_input'}),
+            'imei': forms.NumberInput(attrs={'class': 'form-control form-control-sm', 'id':'imei_input'}),
+            'vendor_address': forms.TextInput(attrs={'class': 'form-control form-control-sm', 'id':'vendor_address_input'}),
+            'total_taxable_amount': forms.TextInput(attrs={'class': 'form-control form-control-sm',  'id': 'vendor_address_input'}),
+            'sgst': forms.TextInput(attrs={'class': 'form-control form-control-sm',  'id': 'vendor_address_input'}),
+            'igst': forms.TextInput(attrs={'class': 'form-control form-control-sm',  'id': 'vendor_address_input'}),
+            'cgst': forms.TextInput(attrs={'class': 'form-control form-control-sm',  'id': 'vendor_address_input'}),
+            'total_amount': forms.TextInput(attrs={'class': 'form-control form-control-sm',  'id': 'vendor_address_input'}),
             'additional_data':forms.TextInput(attrs={'style':'display:none','id':'ad-id'})
             }
